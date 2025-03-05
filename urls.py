ab6f6b4d3941976b6541dc384d6fc2a24baea815
Monayem <mamonayem6@gmail.com>
2025-03-05 19:40:16 +0600

    Done Employe

--- urls.py
+++ urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     path('index/',views.Employee_view,name='employe_index'),
-    path('edit/<int:pk>',views.Employee_edit_view,name='employe_edit'),
+    path('edit/<int:pk>',views.Employee_dashbord_view,name='employe_edit'),
+    path('edit_up/',views.Employee_edit_view,name='employe_create'),
+    path('edit_up/<int:pk>',views.Employee_edit_view,name='employe_edit_up'),
     
 ]
 
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 
 from .models import *
 
@@ -10,7 +10,7 @@ def Employee_view(request):
     }
     return render(request,'employee_app/index.html',contax)
 
-def Employee_edit_view(request,pk):
+def Employee_dashbord_view(request,pk):
     employee=get_object_or_404(Employee,pk=pk)
     print(employee)
 
@@ -18,3 +18,43 @@ def Employee_edit_view(request,pk):
         "employee":employee
     }
     return render(request,'employee_app/edit.html',contax)
+
+
+def Employee_edit_view(request,pk=None):
+    employee = None
+    if pk:
+        employee=get_object_or_404(Employee,pk=pk)
+    else:
+        pk=None
+
+    if request.method=="POST":
+        if pk:
+            employee.first_name=request.POST.get('first_name')
+            employee.last_name=request.POST.get('last_name')
+            employee.degignation=request.POST.get('degignation')
+            employee.email=request.POST.get('email')
+            employee.phone_number=request.POST.get('phone_number')
+            employee.url=request.POST.get('url')
+            if "profile_picture" in request.FILES:
+                employee.image=request.FILES["image"]
+
+            employee.save()
+
+            return redirect("employe_edit", pk=pk)
+        else:
+            new_employee=Employee(
+                first_name=request.POST.get('first_name'),
+                last_name=request.POST.get('last_name'),
+                degignation=request.POST.get('degignation'),
+                email=request.POST.get('email'),
+                phone_number=request.POST.get('phone_number'),
+                url=request.POST.get('url'),
+                image=request.FILES.get("image"),
+            )
+            new_employee.save()
+            return redirect("employe_index")
+        
+    contax={
+        "employee":employee
+    }
+    return render(request,'employee_app/edit_up.html',contax)
